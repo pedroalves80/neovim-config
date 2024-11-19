ba22ee79c6ab139656ea8534ec11267c9b189992
Pedro Miguel Pinheiro Alves <pedro.alves@niw.pt>
2024-11-19 10:19:04 +0000

    feat(statusline): added lualine for github status

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  opts = {
+    options = {
+      theme = 'onedark',
+    },
+  },
+  config = function()
+    local lualine = require 'lualine'
+
+    lualine.setup {
+      sections = {
+        lualine_x = {
+          function()
+            return require('copilot_status').status_string()
+          end,
+          function()
+            return require('copilot_status').enabled()
+          end,
+          'filetype',
+          'fileformat',
+        },
+      },
+    }
+  end,
+}
